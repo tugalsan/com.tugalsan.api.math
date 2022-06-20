e86b854312f0b82017c7f596dd70e5597f7e9dab
tugalsan karabacak <tugalsan@gmail.com>
2022-06-20 23:24:39 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.tugalsan.api.math {
+    requires commons.math3;
+    requires com.tugalsan.api.pack;
+    requires com.tugalsan.api.list;
+    requires com.tugalsan.api.log;
+    requires com.tugalsan.api.string;
+    exports com.tugalsan.api.math.client;
+    exports com.tugalsan.api.math.server;
+}
