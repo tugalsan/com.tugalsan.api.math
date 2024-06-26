eae0f0e057e9b47a6246e3c02f1375ce53c1e8f6
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-26 15:46:36 +0300

    Update TS_MathApproximateUtils.java

--- src/main/java/com/tugalsan/api/math/server/TS_MathApproximateUtils.java
+++ src/main/java/com/tugalsan/api/math/server/TS_MathApproximateUtils.java
@@ -16,10 +16,10 @@ public class TS_MathApproximateUtils {
         var known_output_values = new double[]{8, 18, 50};
         var request_input_values = new double[]{5, 6, 7, 8, 9, 10, 11, 15, 20, 25, 30, 35, 40, 45, 50, 51, 52, 53, 54, 55};
         var calc = TS_MathApproximateUtils.calc(known_input_values, known_output_values, request_input_values);
-        d.ce("test", "TS_MathApproximateUtils.known_input_values", TGS_StringUtils.toString(known_input_values, ", "));
-        d.ce("test", "TS_MathApproximateUtils.known_output_values", TGS_StringUtils.toString(known_output_values, ", "));
-        d.ce("test", "TS_MathApproximateUtils.req ", TGS_StringUtils.toString(request_input_values, ", "));
-        d.ce("test", "TS_MathApproximateUtils.calc", TGS_StringUtils.toString(calc, ", "));
+        d.ce("test", "TS_MathApproximateUtils.known_input_values", TGS_StringUtils.cmn().toString(known_input_values, ", "));
+        d.ce("test", "TS_MathApproximateUtils.known_output_values", TGS_StringUtils.cmn().toString(known_output_values, ", "));
+        d.ce("test", "TS_MathApproximateUtils.req ", TGS_StringUtils.cmn().toString(request_input_values, ", "));
+        d.ce("test", "TS_MathApproximateUtils.calc", TGS_StringUtils.cmn().toString(calc, ", "));
     }
 
     public static double[] calc(double[] known_input_values, double[] known_output_values, double request_input_values[]) {
