a94295273627b484b94b3f693de269ddee2dafb7
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-07-18 12:57:45 +0300

    Update TS_MathCombination.java

--- src/main/java/com/tugalsan/api/math/server/TS_MathCombination.java
+++ src/main/java/com/tugalsan/api/math/server/TS_MathCombination.java
@@ -6,10 +6,6 @@ import org.paukov.combinatorics3.Generator;
 
 public class TS_MathCombination {
 
-    public static <T> List<List<T>> combinations(List<T> arr) {
-        return combinations(arr, arr.size());
-    }
-
     public static <T> List<List<T>> combinations(List<T> arr, int innerItemCount) {
         return Generator.combination(arr)
                 .simple(innerItemCount)
