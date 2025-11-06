ccb819fcb5792c1fd3aa253630910eb58a68f72d
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-11-06 11:04:54 +0300

    Update TS_MathPercentUtils.java

--- src/main/java/com/tugalsan/api/math/server/TS_MathPercentUtils.java
+++ src/main/java/com/tugalsan/api/math/server/TS_MathPercentUtils.java
@@ -2,16 +2,19 @@ package com.tugalsan.api.math.server;
 
 public class TS_MathPercentUtils {
 
+    //DECIMAL
     public static Integer dec(double numerator, double denominator) {
         var val = dbl(numerator, denominator, 0);
         return val == null ? null : val.intValue();
     }
 
+    //DOUBLE
     public static Double dbl(double numerator, double denominator, int precision) {
         var val = str(numerator, denominator, precision);
         return val == null ? null : Double.valueOf(val);
     }
 
+    //STRING
     public static String str(double numerator, double denominator, int precision) {
         if (denominator == 0d) {
             return null;
