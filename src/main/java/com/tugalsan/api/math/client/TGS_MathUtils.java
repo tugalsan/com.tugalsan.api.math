7ef6530765d08280f4296b5be39ce47d340f41d4
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-11-18 15:40:35 +0300

    .

--- src/main/java/com/tugalsan/api/math/client/TGS_MathUtils.java
+++ src/main/java/com/tugalsan/api/math/client/TGS_MathUtils.java
@@ -378,4 +378,11 @@ public class TGS_MathUtils {
                 .max();
     }
 
+    public static double directlyPropotional_dbl(double set1_value1, double set2_value1, double set1_value2) {
+        return set1_value2 * set2_value1 / set1_value1;
+    }
+
+    public static long directlyPropotional_dec(int set1_value1, int set2_value1, int set1_value2) {
+        return Math.round(1d * set1_value2 * set2_value1 / set1_value1);
+    }
 }
